69a389ea4ea3841aec6bec5d28853882f45bba01
AshC1ty <akashbabu.07734@gmail.com>
2024-01-08 18:35:48 +0530

    37:47

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+    return (
+        <form className="login" action="">
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+        </form>
+    )
+}
